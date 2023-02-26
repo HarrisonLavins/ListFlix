15203786bb22038119211276b2c685cb4082d289
Harrison Lavins <hlavins313@gmail.com>
2023-02-26 17:27:56 -0500

    WIP: User home page recommendations

--- app.py
+++ app.py
@@ -6,22 +6,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('users.html')
 
-@app.route('/fetch-movies')
-def fetch_movies():
+@app.route('/home/<string:id>/')
+def fetch_movies(id):
+    # Create object with user information
+    user = {'id': id}
+
     # Use TMDB discover page to fetch random movies from API
-    url = f'https://api.themoviedb.org/3/discover/movie?api_key={api_key}'
+    # url = f'https://api.themoviedb.org/3/discover/movie?api_key={api_key}'
 
+    # Replace this URL to display different API results on the user home page
+    url = 'https://api.themoviedb.org/3/movie/245891/recommendations?api_key=71fd409b351004d729d92c7280888cda&language=en-US&page=1'
+
+    # Get data from API
     response = urllib.request.urlopen(url)
     data = response.read()
     movie_data = json.loads(data)
 
     # Can uncomment below line for debugging, and to see what useful info TMDB API returns
-    # print(dict) 
+    print(movie_data["results"][0]) 
 
-    # Pass the fetch-movies template page a Python dictionary called 'movies'
-    return render_template("fetch-movies.html", movies=movie_data["results"])
+    # Pass the home template page a Python dictionary called 'movies'
+    return render_template("home.html", movies=movie_data["results"], user=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
