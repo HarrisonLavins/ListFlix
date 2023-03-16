f6f5732c11156139678018b23d88a4a8fbcbb1ac
Harrison Lavins <hlavins313@gmail.com>
2023-03-15 21:01:09 -0400

    Cleanup

--- util/printing.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# Pretty print json data to command line
-def pretty_print(json_data):
-    json_object = json.loads(str(json_data))
-
-    json_formatted_str = json.dumps(json_object, indent=2)
-
-    print(json_formatted_str)
